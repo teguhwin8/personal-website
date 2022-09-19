afba349e5de66b17f6aa899cf32cf68f6eb9b9c0
Teguh Widodo <teguhwin8@gmail.com>
2022-09-19 23:39:35 +0700

    Init header component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,28 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import Header from "../components/Header";
 
 const Home: NextPage = () => {
   return (
     <div>
       <Head>
         <title>{"Teguh's Portfolio"}</title>
-        <meta name="description" content="Teguh Widodo Personal Website" />
-        <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className="text-4xl font-extrabold">Hello Teguh</div>
+
+      {/* Header */}
+      <Header />
+
+      {/* Hero */}
+
+      {/* About */}
+
+      {/* Experience */}
+
+      {/* Skils */}
+
+      {/* Projects */}
+
+      {/* Contact Me */}
     </div>
   );
 };
